bf471267464dfd2638a405778c3015784d405d16
Tofaal9152 <tofaal91522@gmail.com>
2025-01-15 04:41:17 +0600

    Template for React Native added tailwindCss and Expo Router

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,20 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Link, Stack } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+    <View className="flex-1 bg-black items-center justify-center">
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View className="p-4 bg-white rounded-lg shadow-md">
+        <Text className="text-xl font-bold text-black mb-4">
+          This screen doesn't exist.
+        </Text>
+        <Link href="/" asChild>
+          <Text className="text-lg font-semibold text-black underline">
+            Go to home screen!
+          </Text>
         </Link>
-      </ThemedView>
-    </>
+      </View>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View>
+      <Text className="bg-red-600  p-3">indexsdsssdsddsdd</Text>
+    </View>
+  );
+};
+
+export default Home;
